f2b7adf76c33adbcd85ebb91baff0bb03cab8117
İbrahim Yüce <ibrahimyucee@outlook.com>
2020-12-18 22:18:15 +0300

    rest01

---
+++ rest01-jackson-databinding-json-demo/src/main/java/com/jackson/json/demo/Driver.java
@@ -0,0 +1,41 @@
+package com.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+
+		try {
+			// create object mapper
+			ObjectMapper mapper = new ObjectMapper();
+
+			// read JSON file and map/convert to Java POJO: data/sample-lite.json
+			Student student = mapper.readValue(new File("data/sample-full.json"), Student.class);
+
+			// print first name and last name
+			System.out.println("First name = " + student.getFirstName());
+			System.out.println("Last name = " + student.getLastName());
+
+			//print out address: street and city
+			Address tempAddress = student.getAddress();
+			System.out.println("Street = " + tempAddress.getStreet());
+			System.out.println("City = " + tempAddress.getCity());
+			
+			//print out languages
+			for(String tempLang : student.getLanguages()) {
+				System.out.println(tempLang);
+			}
+			
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
